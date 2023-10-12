00199b88d47462fcdf19c9515c9c9d03d5fdd068
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-10-12 16:06:52 +0200

    add

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,9 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 console.log('Welcome to Holberton School, what is your name?');
-
-rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-  rl.close();
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString()}`);
 });
 
-rl.on('close', () => {
+process.stdin.on('close', () => {
   console.log('This important software is now closing');
+  process.exit(0);
 });
