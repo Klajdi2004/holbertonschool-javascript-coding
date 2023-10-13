48193a05ec98fd5aec437da502d48180a3232d46
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-10-13 19:40:28 +0200

    add

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const app_port = 1245;
+
+// Create a local app to receive data from
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end("Hello Holberton School!");
+});
+
+console.log(`app listening on localhost:${app_port}`)
+app.listen(app_port);
+module.exports = app;
